2c64c47d00bd4f3e0ae8f40e9e584baac5a14736
Hardwarewise <cryptoatif@gmail.com>
2020-05-25 04:35:08 +0300

    FAQ page for test was created.

--- src/qt/faqpage.h
+++ src/qt/faqpage.h
@@ -2,6 +2,7 @@
 #define FAQPAGE_H
 #include "walletmodel.h"
 #include <QWidget>
+#include <QPushButton>
 
 namespace Ui {
 class FaqPage;
@@ -21,15 +22,33 @@ public:
     explicit FaqPage(const PlatformStyle *platformStyle, QWidget *parent = 0);
     ~FaqPage();
 
+void showEvent(QShowEvent *event) override;
+
 public Q_SLOTS:
+    void setSection(int num);
+private Q_SLOTS:
+    void onFaq1Clicked();
+    void onFaq2Clicked();
+    void onFaq3Clicked();
+    void onFaq4Clicked();
+    void onFaq5Clicked();
+    void onFaq6Clicked();
+    void onFaq7Clicked();
+    void onFaq8Clicked();
+    void onFaq9Clicked();
+    void onFaq10Clicked();
     
 
 private:
     Ui::FaqPage *ui;
     const PlatformStyle *platformStyle;
+    int pos = 0;
 
-
-
+    std::vector<QPushButton*> getButtons();
 };
+
+
+
+
 
 #endif // FAQPAGE_H
