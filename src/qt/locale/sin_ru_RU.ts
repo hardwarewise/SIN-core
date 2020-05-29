90adc8a4a2857f7da40465d6fa015547a6ff3bc4
randomDevSkillz <sebi@luxcore.io>
2020-05-29 09:59:49 +0200

    replace биткоин with SIN

--- src/qt/locale/sin_ru_RU.ts
+++ src/qt/locale/sin_ru_RU.ts
@@ -1127,7 +1127,7 @@
     </message>
     <message>
         <source>Enter a SIN address (e.g. %1)</source>
-        <translation>Введите биткоин-адрес (напр. %1)</translation>
+        <translation>Введите SIN-адрес (напр. %1)</translation>
     </message>
     <message>
         <source>%1 d</source>
