e1a29f8ba8e018c8e67943df139903f017c62789
crptec <crptec.fr@gmail.com>
2020-11-26 16:34:39 +0100

    Revert "[sin] disable status bar"
    This reverts commit 05e01f5ebd724fe9773c326b96f47ce8acdaf484.

--- src/qt/singui.cpp
+++ src/qt/singui.cpp
@@ -40,10 +40,6 @@
 #include <qt/masternodelist.h>
 //
 
-//SIN
-#include <infinitynodetip.h>
-//
-
 // Instaswap
 #include <qt/instaswap.h>
 //
@@ -1533,16 +1529,16 @@ void SINGUI::setNumBlocks(int count, const QDateTime& blockDate, double nVerific
     }
     // Dash
     //else
-    if(infTip.IsFinished())
+    if(!masternodeSync.IsBlockchainSynced())
     {
     //
         QString timeBehindText = GUIUtil::formatNiceTimeOffset(secs);
 
-        progressBarLabel->setVisible(false);
+        progressBarLabel->setVisible(true);
         progressBar->setFormat(tr("%1 behind").arg(timeBehindText));
         progressBar->setMaximum(1000000000);
         progressBar->setValue(nVerificationProgress * 1000000000.0 + 0.5);
-        progressBar->setVisible(false);
+        progressBar->setVisible(true);
 
         tooltip = tr("Catching up...") + QString("<br>") + tooltip;
         if(count != prevBlocks)
