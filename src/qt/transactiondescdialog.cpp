b8afca6f74378eeaa62e66116a7e9729cb31864e
hardwarewise <47283265+hardwarewise@users.noreply.github.com>
2021-01-09 15:10:39 +0300

    [gıi] change transactionsdescdialog color

--- src/qt/transactiondescdialog.cpp
+++ src/qt/transactiondescdialog.cpp
@@ -8,15 +8,23 @@
 #include <qt/transactiontablemodel.h>
 
 #include <QModelIndex>
+#include <QSettings>
+#include <QPushButton>
 
 TransactionDescDialog::TransactionDescDialog(const QModelIndex &idx, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::TransactionDescDialog)
 {
     ui->setupUi(this);
+
+        ui->descFrame->setStyleSheet("background-color: #172F49");
+        ui->detailText->setStyleSheet("QTextEdit {color: white; background-color: transparent; border-radius : 12px; margin: 10px;} QToolTip { color: #000000; background-color: #ffffff; border: 0px;}");
+        ui->buttonBox->button(QDialogButtonBox::Close)->setStyleSheet("border: 1px solid #74B2FE; background-color: #061834; color: #FFFFFF;");
+        
     setWindowTitle(tr("Details for %1").arg(idx.data(TransactionTableModel::TxHashRole).toString()));
     QString desc = idx.data(TransactionTableModel::LongDescriptionRole).toString();
     ui->detailText->setHtml(desc);
+   
 }
 
 TransactionDescDialog::~TransactionDescDialog()
