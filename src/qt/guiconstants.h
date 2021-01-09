998cf4820d604bedf105a705c78d3701bf5ff6d3
hardwarewise <47283265+hardwarewise@users.noreply.github.com>
2021-01-09 14:54:28 +0300

    [gui] change tx status colors

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -20,17 +20,18 @@ static const bool DEFAULT_SPLASHSCREEN = true;
 #define STYLE_INVALID "background:#FF8080"
 
 /* Transaction list -- unconfirmed transaction */
-#define COLOR_UNCONFIRMED QColor(255, 255, 255)
+#define COLOR_UNCONFIRMED QColor(71, 88, 125)
 /* Transaction list -- negative amount */
-#define COLOR_NEGATIVE QColor(255, 255, 255)
+#define COLOR_NEGATIVE QColor(107, 128, 175)
 /* Transaction list -- bare address (without label) */
-#define COLOR_BAREADDRESS QColor(255, 255, 255)
+#define COLOR_BAREADDRESS QColor(76, 93, 130)
 /* Transaction list -- TX status decoration - open until date */
-#define COLOR_TX_STATUS_OPENUNTILDATE QColor(255, 255, 255)
+#define COLOR_TX_STATUS_OPENUNTILDATE QColor(32, 32, 255)
 /* Transaction list -- TX status decoration - danger, tx needs attention */
-#define COLOR_TX_STATUS_DANGER QColor(255, 255, 255)
+#define COLOR_TX_STATUS_DANGER QColor(255, 75, 84)
 /* Transaction list -- TX status decoration - default color */
-#define COLOR_BLACK QColor(255, 255, 255)
+#define COLOR_BLACK QColor(0, 0, 31)
+
 
 /* Tooltips longer than this (in characters) are converted into rich text,
    so that they can be word-wrapped.
