a5b0facb69aa8f8297974079a2a60a5015d8d95b
hardwarewise <47283265+hardwarewise@users.noreply.github.com>
2021-02-01 17:45:07 +0300

    [gui] add new files

---
+++ src/qt/stakepage.cpp
@@ -0,0 +1,28 @@
+#include "stakepage.h"
+#include "forms/ui_stakepage.h"
+#include <qt/platformstyle.h>
+#include <qt/styleSheet.h>
+
+
+
+
+
+StakePage::StakePage(const PlatformStyle *_platformStyle, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::StakePage),
+    platformStyle(_platformStyle)
+{
+    ui->setupUi(this);
+
+
+
+
+}
+
+
+StakePage::~StakePage()
+{
+
+    delete ui;
+}
+
